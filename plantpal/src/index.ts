0e37387c0f18fa9c2499985c4aff0b6b82da8eb9
Connor <connorlwhite@icloud.com>
2025-12-27 16:28:05 -0500

    Switched over to hono framework after watching tutorial as it seems more simlar to previous frameworks I have worked with before.

--- plantpal/src/index.ts
+++ plantpal/src/index.ts
@@ -1,26 +1,18 @@
-import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 
-// Start a Hono app
-const app = new Hono<{ Bindings: Env }>();
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-// Setup OpenAPI registry
-const openapi = fromHono(app, {
-	docs_url: "/",
+app.get("/message", (c) => {
+  return c.text("Hello Hono!");
 });
 
-// Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+app.get("/hello-ai", async (c) => {
+  const results = await c.env.AI.run("@cf/meta/llama-3-8b-instruct", {
+    messages: [
+      {"role": "user", "content": "Say hello in five different languages."}
+    ]
+  })
+  return c.json(results);
+})
 
-// You may also register routes for non OpenAPI directly on Hono
-// app.get('/test', (c) => c.text('Hono!'))
-
-// Export the Hono app
 export default app;
